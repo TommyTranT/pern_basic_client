fdc8fe2fa1c6563f9fffe753ac19dc6459916da6
Tommy Tran <haitran1995@gmail.com>
2023-11-09 22:44:26 -0500

    Add form, input and button

--- src/components/InputName.js
+++ src/components/InputName.js
@@ -1,5 +1,22 @@
+import { Fragment, useState } from "react";
+
 const InputName = () => {
-  return <h1>Input Name</h1>;
+  const [name, setName] = useState("");
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">Pern Name List</h1>
+      <form className="d-flex mt-5">
+        <input
+          type="text"
+          className="form-control"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </Fragment>
+  );
 };
 
 export default InputName;
